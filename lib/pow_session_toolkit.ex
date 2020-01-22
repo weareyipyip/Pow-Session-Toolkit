eb1b65d1539af6ca43405e535d621c186b93ed60
Evert Verboven <e.verboven@yipyip.nl>
2020-01-22 11:31:24 +0100

    project setup files

---
+++ lib/pow_session_toolkit.ex
@@ -0,0 +1,18 @@
+defmodule PowSessionToolkit do
+  @moduledoc """
+  Documentation for PowSessionToolkit.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PowSessionToolkit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pow_session_toolkit_test.exs
@@ -0,0 +1,8 @@
+defmodule PowSessionToolkitTest do
+  use ExUnit.Case
+  doctest PowSessionToolkit
+
+  test "greets the world" do
+    assert PowSessionToolkit.hello() == :world
+  end
+end
