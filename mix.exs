96a11b3045c575addd1892d25f647eb94e6f8d7b
Evert Verboven <e.verboven@yipyip.nl>
2020-02-19 16:32:23 +0100

    Update version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PowSessionToolkit.MixProject do
   def project do
     [
       app: :pow_session_toolkit,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
