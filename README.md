0eee154d61df302fa90cdedd22886f14acb32ef4
Evert Verboven <e.verboven@yipyip.nl>
2020-02-20 14:58:26 +0100

    Bump version number

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `pow_session_toolkit` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:pow_session_toolkit, "~> 0.1.1"}
+    {:pow_session_toolkit, "~> 0.1.2"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PowSessionToolkit.MixProject do
   def project do
     [
       app: :pow_session_toolkit,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
